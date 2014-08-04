074e37a089e967a9aaea11c20ccfc2e77a25469e
wonea <jackbogus@fastmail.fm>
2014-08-04 22:15:52 +0100

    Adjust Readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 SchemaBackup
 ============
+
+Intended as SQL Server backup tool, backup to SVN.
+
+Limitations
+
+Currently only exports DB schema to a working folder, no SVN upload yet
+
+TODO
+
+Upload to SVN
+Manage VPN settings to DB
---
+++ SchemaBackup.Definitions/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaBackup.Definitions
+{
+    public class SchemaSettings
+    {
+        public SchemaSetting[] Settings;
+    }
+
+    public class SchemaSetting
+    {
+        public string WorkingPath { get; set; }
+        public ConnectionCredential ConnCredential { get; set; }
+        public SvnCredential SvnCredential { get; set; }
+    }
+
+    public class ConnectionCredential
+    {
+        public string VpnUsername { get; set; }
+        public string VpnPassword { get; set; }
+        public string DBConnectionStr { get; set; }
+    }
+
+    public class SvnCredential
+    {
+        public string Path { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
